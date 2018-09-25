e883fc6344f975bb7ac5af91c58d6cd7cf8cd774
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2018-09-25 08:59:42 -0700

    Create ez_l2_evpn.py

---
+++ EZ-EVPN/ez_l2_evpn.py
@@ -0,0 +1,75 @@
+import jsonrpclib
+import itertools
+import netaddr
+from cvplibrary import Form
+from cvplibrary import CVPGlobalVariables, GlobalVariableNames
+from cvplibrary import RestClient
+import json
+import re
+
+#GET VARIABLES FROM CVP, USED TO AUTH TO DEVICE.
+ip = CVPGlobalVariables.getValue(GlobalVariableNames.CVP_IP)
+user = CVPGlobalVariables.getValue(GlobalVariableNames.CVP_USERNAME)
+passwd = CVPGlobalVariables.getValue(GlobalVariableNames.CVP_PASSWORD)
+
+cvpserver = 'localhost'
+configlet = 'Compute_VLANs'
+restcall = 'https://'+cvpserver+':443//cvpservice/configlet/getConfigletByName.do?name='+configlet
+#GET VARIABLES FROM THE FORM
+vlanList = []
+vlanNumRegex = re.compile('^([0-9]{1,4})\,.*')
+vlanNameRegex =  re.compile('^[0-9]{1,4}\,(.*)')
+
+def main():
+  
+  #CHECKS THAT ALL FIELDS HAVE DATA
+  #runs API call to grab VLANs
+  client = RestClient(restcall,'GET')
+  if client.connect():
+    configletData = json.loads(client.getResponse())['config']
+    allVlans = configletData.split('\n')
+    vlanList.extend(allVlans)
+
+  #SESSION SETUP FOR eAPI TO DEVICE
+  url = "https://%s:%s@%s/command-api" % (user, passwd, ip)
+  ss = jsonrpclib.Server(url)
+  #CONNECT TO DEVICE
+  response = ss.runCmds( 1, [ 'show ip bgp' ] ) # run command 'show ip bgp' and store output as response.
+  hostname = ss.runCmds( 1, [ 'show hostname' ])[0]['hostname']
+  ASN =  response[0]['vrfs']['default']['asn'] # grab ASN from BGP JSON data.
+  ROUTERID = response[ 0 ]['vrfs']['default']['routerId'] # grab Router-ID from BGP JSON data.
+
+  #Rename Configlet Automatically
+  print '!DynConfig '+hostname+'_AutoVLAN'
+  allVlans = []
+  #Create VLAN
+  for vlan in vlanList:
+    vlanId = vlanNumRegex.match(vlan).group(1)
+    vlanName = vlanNameRegex.match(vlan).group(1)
+    allVlans.append(vlanId)
+    print 'vlan %s' % (vlanId)
+    print ' name %s' % (vlanName)
+    print '!'
+    #Create VLAN Interface
+    print 'interface Vlan %s' % (vlanId)
+    print ' description %s' % (vlanName)
+    print ' shutdown'
+    print '!'
+  #Assign VLAN to VNI
+  print 'interface vxlan1'
+  for vlan in allVlans:
+    print '  vxlan vlan '+vlan+' vni '+vlan
+  print '!'
+  #Create MACVRF for EVPN.
+  print 'router bgp %s' % (ASN)
+  for vlan in allVlans:
+    print '  vlan %s' % (vlan)
+    print '    rd '+ROUTERID+':'+vlan
+    print '    route-target import '+vlan+':'+vlan
+    print '    route-target export '+vlan+':'+vlan
+    print '    redistribute learned'
+    print '    !'
+  print '!'
+
+if __name__ == "__main__":
+    main()
